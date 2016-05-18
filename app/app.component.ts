8e0673956e09d88f37c52ab9f91e94c1df242164
028963692 <028963692@YKM-ENG-047>
2016-05-18 18:00:11 +0300

    gai

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from 'angular2/core';
+
+@Component({
+    selector: 'pm-app',
+    templateUrl:'app/app.component.html'
+})
+
+export class AppComponent {
+    pageTitle: string = 'I added this title';
+}
